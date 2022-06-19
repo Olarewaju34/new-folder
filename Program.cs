4d74c093d0523fda3f0688bcceb44d5542613c7d
Olarewaju34 <mohmzyo@gmail.com>
2022-06-19 07:20:07 +0100

    new folder

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Random random = new Random();
+// int noOfDaysRemaining = random.Next(11);
+// int discount = 0;
+
+
+
+// if ( noOfDaysRemaining == 0)
+// {
+// Console.WriteLine(" Your subscription has expired!");
+// }
+
+
+// else if ( noOfDaysRemaining == 1)
+// {
+//   Console.WriteLine(" Your subscription will expire in a day! ");
+// }
+
+// else if ( noOfDaysRemaining == 2 )
+//  {
+// discount= 20;
+// Console.WriteLine( "Your subscription will expire in" + noOfDaysRemaining + ". Renew now and get " + discount + "% discount " );
+//  } 
+
+// else if ( noOfDaysRemaining <= 5)
+
+// {
+//   discount= 10;
+//   Console.WriteLine(" Your subscription will expire in " + noOfDaysRemaining  + " . Renew now and get" + discount + "% discount");
+// }
+                                        
+
+// if ( noOfDaysRemaining <= 10)
+// {
+//   Console.WriteLine(" Your subscription will soon expire!");
+// }
+
+
+//Console.Write("inpu a number to be multiplied:: ");
+//int n = int.Parse(Console.ReadLine());
+
+///for(int i=1; i<= 12; i++)
+//{
+//Console.WriteLine($"{n} * {i} = {i *n}");
+//}
+
+
+//var phonenum = 07039661664;
+//string num = phonenum.ToString();
+//foreach(char character in num)
+//{
+ //   Console.WriteLine(character);
+//}
+
+
+//string phonenum = "08195353244";
+ 
+// foreach(char num in phonenum)
+ //{
+   // if(num%2 != 0)
+   // {
+     //   Console.WriteLine(num);
+   // }
+   
+
+    
+ //}
+
+
